c1fdbb3d6e1e07f5416dfd0f65ba57695a75be34
ArlanDaulen <arlanvladelets@gmail.com>
2022-03-20 18:41:12 +0600

    Arlan's commit

---
+++ lib/features/profile/domain/entities/number_trivia.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class NumberTrivia extends Equatable{
+  String? text;
+  int? number;
+  bool? found;
+  String? type;
+
+  NumberTrivia({this.text, this.number, this.found, this.type});
+
+  NumberTrivia.fromJson(Map<String, dynamic> json) {
+    text = json['text'];
+    number = json['number'];
+    found = json['found'];
+    type = json['type'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['text'] = this.text;
+    data['number'] = this.number;
+    data['found'] = this.found;
+    data['type'] = this.type;
+    return data;
+  }
+}
